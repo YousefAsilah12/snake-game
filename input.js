6fde81b9cee1b4b92ab6be043a99ce5835fd6e30
Yous3f <yasilah4@gmail.com>
2023-01-30 19:28:24 +0200

    update -touch

--- input.js
+++ input.js
@@ -1,35 +1,70 @@
+const gameContainer = document.querySelector('#game-board');
+
 let inputDirection = {
   x: 0,
   y: 0
+};
+let lastInputDirection = {
+  x: 0,
+  y: 0
+};
+
+gameContainer.addEventListener('touchstart', handleTouchStart);
+gameContainer.addEventListener('touchmove', handleTouchMove);
+
+function handleTouchStart(event) {
+  event.preventDefault();
+  const touch = event.touches[0];
+  startX = touch.clientX;
+  startY = touch.clientY;
 }
-let lastInputDirection = {x:0,y:0}
- 
 
-window.document.addEventListener('keydown', (e) => {
-  switch (e.key) {
-    case 'ArrowUp':
-      if(lastInputDirection.y!==0) break;//cant move up whil moveing down
-      inputDirection = {x:0,y: -1};
-      break;
-    case 'ArrowDown':
-      if(lastInputDirection.y!==0) break;////cant move up whil moveing down
-      inputDirection= {x:0,y: 1};
-      break
-    case 'ArrowLeft':
-      if(lastInputDirection.x!==0) break;//cant move left whil moveing right
+function handleTouchMove(event) {
+  event.preventDefault();
+  const touch = event.touches[0];
+  const touchX = touch.clientX;
+  const touchY = touch.clientY;
+  const diffX = touchX - startX;
+  const diffY = touchY - startY;
 
-      inputDirection= {x:-1,y: 0};
-      break
-    case 'ArrowRight':
-      if(lastInputDirection.x!==0) break;//cant move left whil moveing right
-
-      inputDirection= {x:1,y: 0};
-      break
-
+  if (Math.abs(diffX) > Math.abs(diffY)) {
+    // Horizontal swipe
+    if (diffX > 0) {
+      // Move right
+      if (lastInputDirection.x !== 0) return;
+      inputDirection = {
+        x: 1,
+        y: 0
+      };
+    } else {
+      // Move left
+      if (lastInputDirection.x !== 0) return;
+      inputDirection = {
+        x: -1,
+        y: 0
+      };
+    }
+  } else {
+    // Vertical swipe
+    if (diffY > 0) {
+      // Move down
+      if (lastInputDirection.y !== 0) return;
+      inputDirection = {
+        x: 0,
+        y: 1
+      };
+    } else {
+      // Move up
+      if (lastInputDirection.y !== 0)return;
+      inputDirection = {
+        x: 0,
+        y: -1
+      };
+    }
   }
+}
 
-});
-export function getIputDirection() {
-  lastInputDirection=inputDirection;
-  return inputDirection
+export function getInputDirection() {
+  lastInputDirection = inputDirection;
+  return inputDirection;
 }
